e4bde19e7d3b4f76543917dfe6c18cf44d9c2b4e
Harun-or-rashid <harun35-2093@diu.edu.bd>
2023-01-16 16:10:06 +0600

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,4 @@
+function HomePage(){
+return <h1>The Home Pahge</h1>
+}
+export default HomePage;
---
+++ pages/news/index.js
@@ -0,0 +1,4 @@
+function NewsPage(){
+    return <h1>The News Pahge</h1>
+    }
+    export default NewsPage;
