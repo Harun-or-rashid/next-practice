b6c376a509397f832c3f0e3c9b18da529dbf26ba
Harun-or-rashid <Harun-or-rashid>
2023-01-21 15:43:03 +0600

    serverside generation

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import {useRouter} from "next/router";
 
-function HomePage(){
+function HomePage({news}){
     const router=useRouter()
     const clickHandler=()=>{
         router.push('/blog')
@@ -15,5 +15,25 @@ function HomePage(){
             </li>
         <button type="button" onClick={clickHandler}> Blogs</button>
         </ul>
+                <div>
+                    {news.map(news => {
+
+                        return(
+                            <div>
+                                <h1 key={news.id}><p>{news.title}</p></h1>
+                            </div>
+                        )
+                    })}
+                </div>
     </div>)}
 export default HomePage;
+export  async function getStaticProps(){
+    const res=await fetch('https://jsonplaceholder.typicode.com/posts')
+    const news=await res.json();
+    console.log(news)
+    return{
+        props:{
+            news:news
+        }
+        }
+}
