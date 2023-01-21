1d57883a5ec49b1f5b35f1db62e9be82d8e5024b
Harun-or-rashid <Harun-or-rashid>
2023-01-21 13:56:41 +0600

    event router

---
+++ pages/blog/index.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+function Blog() {
+    return (<div>
+        <ul>
+            <li>
+                <Link href="/">Home</Link>
+            </li>
+        </ul>
+    </div>)
+}
+ export default Blog
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,19 @@
+import Link from "next/link";
+import {useRouter} from "next/router";
+
 function HomePage(){
-return <h1>The Home Pahge</h1>
-}
+    const router=useRouter()
+    const clickHandler=()=>{
+        router.push('/blog')
+    }
+    return (<div>
+        <ul>
+            <li>
+                <Link href="/news">
+                        News
+                </Link>
+            </li>
+        <button type="button" onClick={clickHandler}> Blogs</button>
+        </ul>
+    </div>)}
 export default HomePage;
